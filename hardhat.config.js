a32d1d089209f49afba08a6a2b8aa236ef92b293
hackr25 <83638637+hackr25@users.noreply.github.com>
2024-08-19 18:36:37 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,11 +3,18 @@ require('@openzeppelin/hardhat-upgrades');
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.20",
-  networks: {
-    swisstronik: {
-      url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
-    },
+  // ...rest of the config...
+  etherscan: {
+    apiKey: `ANY_STRING_WILL_DO`,
+    customChains: [
+      {
+        network: "swisstronik",
+        chainId: 1291,
+        urls: {
+          apiURL: "https://explorer-evm.testnet.swisstronik.com/api",
+          browserURL: "https://explorer-evm.testnet.swisstronik.com",
+        },
+      },
+    ],
   },
 };
